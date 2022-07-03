9152e1bfe0688192eeb113e4c905f337d1cb668f
Mikel Ruiz saenz <mruiz-sa@c3r7s4.42urduliz.com>
2022-07-03 18:30:02 +0200

    42_Exam_Rank_02 Exercises

--- Level_02/ft_strcmp/ft_strcmp.c
+++ Level_02/ft_strcmp/ft_strcmp.c
@@ -3,7 +3,13 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		if (s1[i] > s2[i])
+			return (1);
 		i++;
-	return (s1[i] - s2[i]);
+	}
+	return (0);
 }
