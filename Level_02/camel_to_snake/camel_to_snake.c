e217b26f18358605ee4e232c5bb5a1360c4c7d36
Mikel Ruiz saenz <mruiz-sa@c3r7s4.42urduliz.com>
2022-07-03 16:42:25 +0200

    42_Exam_Rank_02 Exercises

--- Level_02/camel_to_snake/camel_to_snake.c
+++ Level_02/camel_to_snake/camel_to_snake.c
@@ -7,6 +7,12 @@ int	main(int ac, char *av[])
 	i = 0;
 	if (ac == 2)
 	{
+		if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+		{
+			av[1][i] = av[1][i] + 32;
+			write(1, &av[1][i], 1);
+			i++;
+		}
 		while (av[1][i])
 		{
 			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
