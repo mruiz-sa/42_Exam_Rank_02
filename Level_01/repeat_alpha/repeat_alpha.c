963bbfaf6c8f7500db11c348fddf77e58b38dbdb
Mikel Ruiz saenz <mruiz-sa@c2r6s5.42urduliz.com>
2022-09-02 10:48:52 +0200

    repeat alpha fixed

--- Level_01/repeat_alpha/repeat_alpha.c
+++ Level_01/repeat_alpha/repeat_alpha.c
@@ -15,6 +15,8 @@ int	main(int ac, char *av[])
 				count = av[1][i] - 96;
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 				count = av[1][i] - 64;
+			else
+				write(1, &av[1][i], 1);
 			while (count)
 			{
 				write(1, &av[1][i], 1);
